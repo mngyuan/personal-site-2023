a264881063326343f0cbd616cb30fd1411aac4f8
Kevin Lee <hikevinmlee@gmail.com>
2024-08-15 18:27:57 +0100

    flesh out dreaming page a bit more

--- src/pages/dreaming.js
+++ src/pages/dreaming.js
@@ -7,6 +7,8 @@ import dreamingPic2 from 'public/047A2008.webp';
 import dreamingPic3 from 'public/IMG_7871.webp';
 import dreamingPic4 from 'public/Screen Shot 2022-02-08 at 9.27.13 PM.webp';
 import dreamingPic5 from 'public/afterword.webp';
+import dreamingPic6 from 'public/129283090033.webp';
+import dreamingPic7 from 'public/2390-36.webp';
 
 const Dreaming = () => {
   return (
@@ -38,11 +40,36 @@ const Dreaming = () => {
         left={
           <Image
             className="h-full w-full object-cover rounded-md"
-            src={dreamingPic3}
+            src={dreamingPic6}
             alt="MX-CLP, a new video editing interface"
           />
         }
       ></Row>
+      <Row
+        left={
+          <Image
+            className="h-full w-full object-cover rounded-md"
+            src={dreamingPic7}
+            alt="MX-CLP, a new video editing interface"
+          />
+        }
+      ></Row>
+      <Row />
+      <Row
+        left={
+          <Image
+            className="h-full w-full object-cover rounded-md"
+            src={dreamingPic3}
+            alt="MX-CLP, a new video editing interface"
+          />
+        }
+      >
+        DREAMING was featured at the Tokyo Art Book Fair in 2022 at the Museum
+        of Contemporary Art in Tokyo.
+        <br />
+        <br />
+        This project was deeply personal, containing reflections on my heritage.
+      </Row>
       <Row
         left={
           <Image
@@ -53,26 +80,21 @@ const Dreaming = () => {
         }
         final
       >
-        DREAMING was featured at the Tokyo Art Book Fair in 2022 at the Museum
-        of Contemporary Art in Tokyo.
-        <br />
-        <br />
-        This project was deeply personal, containing reflections on my heritage.
-        <br />
-        <br />
         Excerpt from the foreword:
       </Row>
       <Row>
-        <br />
-        <br />I spent two months wandering Asia alone in 2019, meditating in the
-        incredibly personal mental space a language and culture barrier creates.
-        Crowded public spaces took on a dream-like quality, as if they existed
-        solely for myself as places to both confront and embrace a surreal
-        familiarity - the familiarity of a First Home, experienced through
-        memories of color and spice and foreign tongues spoken in adopted homes.
-        The East is at once immediate and inaccessible, a place imagined but
-        never fathomed in its whole, its realities and complexities a sensory
-        overload for those who have long walked the lonesome path of diaspora.
+        <quote className="italic">
+          <br />I spent two months wandering Asia alone in 2019, meditating in
+          the incredibly personal mental space a language and culture barrier
+          creates. Crowded public spaces took on a dream-like quality, as if
+          they existed solely for myself as places to both confront and embrace
+          a surreal familiarity - the familiarity of a First Home, experienced
+          through memories of color and spice and foreign tongues spoken in
+          adopted homes. The East is at once immediate and inaccessible, a place
+          imagined but never fathomed in its whole, its realities and
+          complexities a sensory overload for those who have long walked the
+          lonesome path of diaspora.
+        </quote>
       </Row>
       <Row
         left={
@@ -84,13 +106,15 @@ const Dreaming = () => {
         }
         final
       >
-        Suddenly I found myself overwhelmed with a litany of colors, each one
-        more resonant than the last, their understated vibrancies completing
-        empty chords within me: the jade of faded ads, the soft filtered gold of
-        the Asian sun, the screams of neon, the gaudy brilliance of bus sides
-        and bus seats, the piercing blue of the shadows cast by salarymen and
-        their briefcases, the eggshell white and cream of the lines drawn on the
-        regal Toyota Crown.
+        <quote className="italic">
+          Suddenly I found myself overwhelmed with a litany of colors, each one
+          more resonant than the last, their understated vibrancies completing
+          empty chords within me: the jade of faded ads, the soft filtered gold
+          of the Asian sun, the screams of neon, the gaudy brilliance of bus
+          sides and bus seats, the piercing blue of the shadows cast by
+          salarymen and their briefcases, the eggshell white and cream of the
+          lines drawn on the regal Toyota Crown.
+        </quote>
       </Row>
     </SharedLayout>
   );
